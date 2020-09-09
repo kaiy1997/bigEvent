56513ed4ef2d96a7fb65a9f8d6606d0e567ff92d
kaiy <973280033@qq.com>
2020-09-09 17:20:21 +0800

    登录和注册校验规则完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,7 +9,18 @@ $(function () {
         $('.login').show()
         $('.reg').hide()
     })
-    $('.layui-form').on('submit',function(e){
-       console.log(e);
+    //表单验证规则
+    var form = layui.form
+    form.verify({
+        pass: [
+            /^[\S]{6,12}$/,
+            '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value) {
+            var pwd = $('.reg [name=password]').val()
+            if (pwd !== value) {
+                return '两次输入的密码不一样'
+            }
+        }
     })
 })
