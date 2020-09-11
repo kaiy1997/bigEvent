6694401b1b6d990f80b7367697ecfc49faaa6055
kaiy <973280033@qq.com>
2020-09-11 11:14:50 +0800

    登录页面完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,13 +4,17 @@ $(function () {
         $('.reg').show()
         $('.login').hide()
     })
+
     // 点击登录按钮
     $('#login_btn').on('click', function () {
         $('.login').show()
         $('.reg').hide()
     })
-    //表单验证规则
+
     var form = layui.form
+    var layer = layui.layer
+
+    //表单验证规则
     form.verify({
         pass: [
             /^[\S]{6,12}$/,
@@ -22,5 +26,44 @@ $(function () {
                 return '两次输入的密码不一样'
             }
         }
+    })
+
+    // 注册功能
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+
+        $.post('/api/reguser', {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }, function (res) {
+
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+
+            layer.msg('注册成功，请登录！')
+
+            $('#login_btn').click()
+        })
+
+    })
+
+    // 登录功能
+    $('#form_login').submit(function (e) {
+        e.preventDefault()
+
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    layer.msg(res.message)
+                }
+
+                localStorage.setItem('token', res.token)
+
+            }
+        })
     })
 }) 
