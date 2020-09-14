15df8c73b6f861cc09db95a44df15c159a5b0fdd
kaiy <973280033@qq.com>
2020-09-14 15:38:40 +0800

    文章列表完成

---
+++ assets/js/acticle/art_list.js
@@ -0,0 +1,111 @@
+$(function () {
+  var form = layui.form
+  var layer = layui.layer
+  var total = 0 // 数据总数
+  var q = {
+    pagenum: 1, // 页码值
+    pagesize: 2, // 每页显示的数据
+    cate_id: '', // 文章分类的 Id
+    state: '', // 文章的状态
+  }
+
+  // 渲染分类文章
+  xrArt()
+  function xrArt() {
+    $.ajax({
+      method: 'GET',
+      url: '/my/article/list',
+      data: q,
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg(res.message)
+        }
+        // 数据总数
+        total = res.total
+        var tr = template('tpl_tr', res)
+        $('tbody').html(tr)
+        // 渲染分页
+        xrpage()
+      }
+    })
+
+  }
+
+  // 渲染所有分类
+  xrList()
+  function xrList() {
+    $.ajax({
+      method: 'GET',
+      url: '/my/article/cates',
+      // data: q,
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg(res.message)
+        }
+        var tr = template('tpl_list', res)
+        $('[name=cate_id]').html(tr)
+        form.render()
+      }
+    })
+
+  }
+
+  // 筛选功能
+  $('#selectList').on('submit', function (e) {
+    e.preventDefault()
+    var newCate_id = $('[name=cate_id]').val()
+    var newState = $('[name=state]').val()
+    q.cate_id = newCate_id
+    q.state = newState
+    xrArt()
+  })
+
+  // 分页 
+  function xrpage() {
+    layui.use('laypage', function () {
+      var laypage = layui.laypage;
+      //执行一个laypage实例
+      laypage.render({
+        elem: 'test1',
+        count: total, // 数据总数
+        limit: q.pagesize, // 每页显示的数据
+        limits: [2, 3, 5, 10],//数据总数，从服务端得到
+        curr: q.pagenum, // 页码值
+        layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+        jump: function (obj, first) {
+          q.pagenum = obj.curr
+          q.pagesize = obj.limit
+          //首次不执行
+          if (!first) {
+            xrArt()
+          }
+        }
+      })
+    })
+  }
+
+  // 删除功能
+  $('body').on('click', '#removeBtn', function () {
+    // 文章的Id
+    var removeId = $(this).attr('removeId')
+    layer.confirm('确认删除文章?', function (index) {
+      $.ajax({
+        method: 'GET',
+        url: `/my/article/delete/${removeId}`,
+        success: function (res) {
+          if (res.status !== 0) {
+            return layer.msg(res.message)
+          }
+          layer.msg(res.message)
+          xrArt()
+        }
+      })
+      // 关闭提示框
+      layer.close(index);
+    })
+
+  })
+
+  
+
+})
